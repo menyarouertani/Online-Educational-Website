0c5bdd264f77292cbb8e4c3a27505f0f608d79f9
ghozlene <maghozlene@clevory.com>
2023-08-15 11:40:29 +0100

    fixing bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,37 @@
-import React, { Fragment } from "react";
-import Header from "../components/Header/Header";
-import HeroSection from "../components/Hero-Section/HeroSection";
+import React, { Fragment } from 'react';
+import Header from '../components/Header/Header';
+import HeroSection from '../components/Hero-Section/HeroSection';
 
+import AboutUs from '../components/About-us/AboutUs';
+import Courses from '../components/Courses-section/Courses';
+import ChooseUs from '../components/Choose-us/ChooseUs';
+import Features from '../components/Feature-section/Features';
+import FreeCourse from '../components/Free-course-section/FreeCourse';
 
-import AboutUs from "../components/About-us/AboutUs";
-import Courses from "../components/Courses-section/Courses";
-import ChooseUs from "../components/Choose-us/ChooseUs";
-import Features from "../components/Feature-section/Features";
-import FreeCourse from "../components/Free-course-section/FreeCourse";
-
-import Testimonials from "../components/Testimonial/Testimonials";
-import MyForm from "../components/subscription/form";
-import Newsletter from "../components/Newsletter/Newsletter";
-import Footer from "../components/Footer/Footer";
-import FormContainer from "../components/subscription/form";
-import CombinedLayout from "../components/layoutcomb/combinedlayout";
+import Testimonials from '../components/Testimonial/Testimonials';
+import MyForm from '../components/subscription/form';
+import Newsletter from '../components/Newsletter/Newsletter';
+import Footer from '../components/Footer/Footer';
+import FormContainer from '../components/subscription/form';
+import CombinedLayout from '../components/Hero-Section/HeroSection';
 
 const Home = () => {
-  return (
-    <Fragment>
-      <Header />
-      < CombinedLayout/>
-      <HeroSection />
-      < FormContainer/> 
-      <AboutUs /> 
-      <Courses />
-      <ChooseUs />
-      <Features />
-      <FreeCourse />
-      <Testimonials />
-      <Newsletter />
-      <Footer /> 
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Header />
+			<CombinedLayout />
+			<HeroSection />
+			<FormContainer />
+			<AboutUs />
+			<Courses />
+			<ChooseUs />
+			<Features />
+			<FreeCourse />
+			<Testimonials />
+			<Newsletter />
+			<Footer />
+		</Fragment>
+	);
 };
 
 export default Home;
