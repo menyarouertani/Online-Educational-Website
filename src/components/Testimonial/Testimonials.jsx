5e211cfff34d4b4556a54ffaa8f01e0fd5b9b638
ghozlene <maghozlene@clevory.com>
2023-08-21 11:51:37 +0100

    fixing the problem of the png image

--- src/components/Testimonial/Testimonials.jsx
+++ src/components/Testimonial/Testimonials.jsx
@@ -1,89 +1,74 @@
-import React from "react";
-import "./testimonial.css";
-import { Container, Row, Col } from "reactstrap";
-import Slider from "react-slick";
+import React from 'react';
+import './testimonial.css';
+import { Container, Row, Col } from 'reactstrap';
+import Slider from 'react-slick';
 
-import img from "../../src/assets/images/photofeedback.png";
+import img from '../../assests/images/photofeedback.png';
 
 const Testimonials = () => {
-  const settings = {
-    infinite: true,
-    dots: true,
-    speed: 500,
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    slidesToScroll: 1,
-  };
-  return (
-    <section>
-      <Container>
-        <Row>
-          <Col lg="10" md="12" className="m-auto">
-            <div className="testimonial__wrapper d-flex justify-content-between align-items-center ">
-              <div className="testimonial__img w-50">
-                <img src={img} alt="" className="w-100" />
-              </div>
+	const settings = {
+		infinite: true,
+		dots: true,
+		speed: 500,
+		slidesToShow: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		slidesToScroll: 1,
+	};
+	return (
+		<section>
+			<Container>
+				<Row>
+					<Col lg='10' md='12' className='m-auto'>
+						<div className='testimonial__wrapper d-flex justify-content-between align-items-center '>
+							<div className='testimonial__img w-50'>
+								<img src={img} alt='' className='w-100' />
+							</div>
 
-              <div className="testimonial__content w-50">
-                <h2 className="mb-4">Temoignages des étudiants </h2>
+							<div className='testimonial__content w-50'>
+								<h2 className='mb-4'>Temoignages des étudiants </h2>
 
-                <Slider {...settings}>
-                  <div>
-                    <div className="single__testimonial">
-                      <h6 className="mb-3 fw-bold">
-                        
-                      </h6>
-                      <p>
-                        très bien 
-                      </p>
+								<Slider {...settings}>
+									<div>
+										<div className='single__testimonial'>
+											<h6 className='mb-3 fw-bold'></h6>
+											<p>très bien</p>
 
-                      <div className="student__info mt-4">
-                        <h6 className="fw-bold">MR A </h6>
-                        
-                      </div>
-                    </div>
-                  </div>
+											<div className='student__info mt-4'>
+												<h6 className='fw-bold'>MR A </h6>
+											</div>
+										</div>
+									</div>
 
-                  <div>
-                    <div className="single__testimonial">
-                      <h6 className="mb-3 fw-bold">
-                        
-                      </h6>
-                      <p>
-                      parfait 
-                      </p>
+									<div>
+										<div className='single__testimonial'>
+											<h6 className='mb-3 fw-bold'></h6>
+											<p>parfait</p>
 
-                      <div className="student__info mt-4">
-                        <h6 className="fw-bold">MR x </h6>
-                      
-                      </div>
-                    </div>
-                  </div>
+											<div className='student__info mt-4'>
+												<h6 className='fw-bold'>MR x </h6>
+											</div>
+										</div>
+									</div>
 
-                  <div>
-                    <div className="single__testimonial">
-                      <h6 className="mb-3 fw-bold">
-                        
-                      </h6>
-                      <p>
-                     perfect
-                      </p>
+									<div>
+										<div className='single__testimonial'>
+											<h6 className='mb-3 fw-bold'></h6>
+											<p>perfect</p>
 
-                      <div className="student__info mt-4">
-                        <h6 className="fw-bold">MR Y </h6>
-                        
-                      </div>
-                    </div>
-                  </div>
-                </Slider>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  );
+											<div className='student__info mt-4'>
+												<h6 className='fw-bold'>MR Y </h6>
+											</div>
+										</div>
+									</div>
+								</Slider>
+							</div>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	);
 };
 
 export default Testimonials;
